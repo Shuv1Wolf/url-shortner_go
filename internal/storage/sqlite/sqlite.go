d93e42d711caec0d206855a00865b38418efc9c0
Shuv1Wolf <mskuratik06@gmail.com>
2023-11-22 13:27:54 +0300

    add storage

---
+++ internal/storage/sqlite/sqlite.go
@@ -0,0 +1,39 @@
+package sqlite
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+func New(storagePath string) (*Storage, error) {
+	const op = "stotage.sqlite.New"
+
+	db, err := sql.Open("sqlite3", storagePath)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	smtp, err := db.Prepare(`
+	CREATE TABLE IF NOT EXIST url(
+		id INTEGER PRIMARY KEY,
+		alias TEXT NOT NULL UNIQUE,
+		url TEXT NOT NULL);
+	CREATE INDEX IF NOT EXISTS idx_alias ON url(alias);
+	`)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	_, err = smtp.Exec()
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &Storage{db: db}, nil
+}
