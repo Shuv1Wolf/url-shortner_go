0806d0f940f41f54e516caa44e278a69d92782ca
Shuv1Wolf <mskuratik06@gmail.com>
2023-12-05 00:10:20 +0300

    add delete

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"os"
 	"url-shortener/internal/config"
+	"url-shortener/internal/http-server/handlers/delete"
 	"url-shortener/internal/http-server/handlers/redirect"
 	"url-shortener/internal/http-server/handlers/url/save"
 	mwLogger "url-shortener/internal/http-server/middleware/logger"
@@ -50,6 +51,7 @@ func main() {
 
 	router.Post("/url", save.New(log, storage))
 	router.Get("/{alias}", redirect.New(log, storage))
+	router.Delete("/{alias}", delete.New(log, storage))
 
 	log.Info("starting server", slog.String("Address", cfg.Address))
 	srv := &http.Server{
--- internal/http-server/handlers/delete/delete.go
+++ internal/http-server/handlers/delete/delete.go
@@ -1,4 +1,4 @@
-package redirect
+package delete
 
 import (
 	"errors"
@@ -22,13 +22,13 @@ type Response struct {
 	Alias string `json:"alias,omitempty"`
 }
 
-type GetterURL interface {
-	GetURL(alias string) (string, error)
+type DelURL interface {
+	DeleteURL(alias string) (string, error)
 }
 
-func New(log *slog.Logger, getterUrl GetterURL) http.HandlerFunc {
+func New(log *slog.Logger, delUrl DelURL) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		const op = "handlers.redirect.redirect.New"
+		const op = "handlers.delete.delete.New"
 
 		log = log.With(
 			slog.String("op", op),
@@ -44,7 +44,7 @@ func New(log *slog.Logger, getterUrl GetterURL) http.HandlerFunc {
 			return
 		}
 
-		resURL, err := getterUrl.GetURL(alias)
+		resURL, err := delUrl.DeleteURL(alias)
 		if errors.Is(err, storage.ErrURLNotFound) {
 			log.Info("url not found", "alias", alias)
 
@@ -54,15 +54,18 @@ func New(log *slog.Logger, getterUrl GetterURL) http.HandlerFunc {
 		}
 
 		if err != nil {
-			log.Error("failed to get url", sl.Err(err))
+			log.Error("failed to delete url", sl.Err(err))
 
 			render.JSON(w, r, resp.Error("internal error"))
 
 			return
 		}
 
-		log.Info("got url", slog.String("url", resURL))
+		log.Info("delete url and alias", slog.String("url", resURL), slog.String("alias", alias))
 
-		http.Redirect(w, r, resURL, http.StatusFound)
+		render.JSON(w, r, Response{
+			Response: resp.OK(),
+			Alias:    alias,
+		})
 	}
 }
